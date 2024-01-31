c84575e252e565b22e79b8f685b263bda12ba223
hayyaoe <hayya947@gmail.com>
2024-01-31 07:52:46 +0700

    player done

--- resources/views/player/layouts/app.blade.php
+++ resources/views/player/layouts/app.blade.php
@@ -2,8 +2,6 @@
 @include('player.layouts.header')
     <title>@yield('title')</title>
         @yield('script')
-        @livewireStyles
-        @vite('resources/js/app.js')
 </head>
 <body>
     @yield('content')
